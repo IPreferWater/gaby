ffb12099be7928e05e383148547bb18610339b1d
IPreferWater <ipreferwater2@gmail.com>
2022-11-28 17:28:56 +0100

    feat(dev): adapt childScore to new format for RowTabs

--- lib/child_score.dart
+++ lib/child_score.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:gaby/international/t.dart';
 import 'package:gaby/row_tab.dart';
+import 'package:gaby/const/const.dart';
+import 'package:gaby/widgets/score.dart';
 
 class TodoName {
   String title;
@@ -40,101 +45,84 @@ class _ChildScoreState extends State<ChildScore> {
 
   @override
   Widget build(BuildContext context) {
-    List<TodoName> todoNames = [
-      TodoName(
-        title: "Ascite",
-        rowTabOpts: [
-          RowTabOpts(title: "absent", value: 1),
-          RowTabOpts(title: "modérée", value: 2),
-          RowTabOpts(title: "tendu", value: 3),
-        ],
-        onValueChanged: (int value) {
-          setState(() {
-            _ascite = value;
-          });
-        },
-      ),
-      TodoName(
-        title: "Bilirubine (µmol/l)",
-        rowTabOpts: [
-          RowTabOpts(title: "<35", value: 1),
-          RowTabOpts(title: "35-50", value: 2),
-          RowTabOpts(title: ">50", value: 3)
-        ],
-        onValueChanged: (int value) {
-          setState(() {
-            _bilirubine = value;
-          });
-        },
-      ),
-      TodoName(
-        title: "Albumine (g/l)",
-        rowTabOpts: [
-          RowTabOpts(title: ">35", value: 1),
-          RowTabOpts(title: "28-35", value: 2),
-          RowTabOpts(title: "<28", value: 3)
-        ],
-        onValueChanged: (int value) {
-          setState(() {
-            _albumine = value;
-          });
-        },
-      ),
-      TodoName(
-        title: "INR (TP)",
-        rowTabOpts: [
-          RowTabOpts(title: "<1.7 (>50%)", value: 1),
-          RowTabOpts(title: "1.7-2.2 (40-50%)", value: 2),
-          RowTabOpts(title: ">2.2 (<40%)", value: 3)
-        ],
-        onValueChanged: (int value) {
-          setState(() {
-            _inr_tp = value;
-          });
-        },
-      ),
-      TodoName(
-        title: "Encéphalopathie",
-        rowTabOpts: [
-          RowTabOpts(title: "Absente", value: 1),
-          RowTabOpts(title: "Légère à modérée (stade 1-2)", value: 2),
-          RowTabOpts(title: "Sévère (stade 3-4)", value: 3)
-        ],
-        onValueChanged: (int value) {
-          setState(() {
-            _encephalopathie = value;
-          });
-        },
-      )
-    ];
-
     return Scaffold(
       appBar: AppBar(title: const Text("Child score")),
       body: Column(
         children: [
-          // if are values are set, show the score
-          isAllValuesSet() == true
-              ? Text('Child score : ${calculateScore()}',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue,
-                      fontSize: 50))
-              //if some values are missing, display warning message
-              : Text('Child score : remplissez tous les champs',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.orange,
-                      fontSize: 30)),
-          /*Expanded(
-            child: ListView.builder(
-                itemCount: todoNames.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return RowTab(
-                      title: todoNames[index].title,
-                      options: todoNames[index].rowTabOpts,
-                      onValueChanged: todoNames[index].onValueChanged);
-                }),
-          )*/
+          Score(
+              title: "Child score",
+              score: calculateScore(),
+              allValueSet: isAllValuesSet()),
+          Expanded(
+              child: ListView(
+            children: [
+              RowTab(
+                  title: ASCITE.map[Languages.fr]!,
+                  vvv: ASCITE,
+                  options: [
+                    RowTabOpts(title: "absent", value: 1),
+                    RowTabOpts(title: "modérée", value: 2),
+                    RowTabOpts(title: "tendu", value: 3),
+                  ],
+                  onValueChanged: (int value) {
+                    setState(() {
+                      _ascite = value;
+                    });
+                  }),
+              RowTab(
+                  title: BILBURINE.map[Languages.fr]!,
+                  vvv: BILBURINE,
+                  options: [
+                    RowTabOpts(title: "<35", value: 1),
+                    RowTabOpts(title: "35-50", value: 2),
+                    RowTabOpts(title: ">50", value: 3)
+                  ],
+                  onValueChanged: (int value) {
+                    setState(() {
+                      _bilirubine = value;
+                    });
+                  }),
+              RowTab(
+                  title: ALBUMINE.map[Languages.fr]!,
+                  vvv: ALBUMINE,
+                  options: [
+                    RowTabOpts(title: ">35", value: 1),
+                    RowTabOpts(title: "28-35", value: 2),
+                    RowTabOpts(title: "<28", value: 3)
+                  ],
+                  onValueChanged: (int value) {
+                    setState(() {
+                      _albumine = value;
+                    });
+                  }),
+              RowTab(
+                  title: '${INR.map[Languages.fr]!} (TP)',
+                  vvv: INR,
+                  options: [
+                    RowTabOpts(title: "<1.7 (>50%)", value: 1),
+                    RowTabOpts(title: "1.7-2.2 (40-50%)", value: 2),
+                    RowTabOpts(title: ">2.2 (<40%)", value: 3)
+                  ],
+                  onValueChanged: (int value) {
+                    setState(() {
+                      _inr_tp = value;
+                    });
+                  }),
+              RowTab(
+                  title: '${ENCEPHALOPATHIE.map[Languages.fr]!} (TP)',
+                  vvv: ENCEPHALOPATHIE,
+                  options: [
+                    RowTabOpts(title: "Absente", value: 1),
+                    RowTabOpts(title: "Légère à modérée (stade 1-2)", value: 2),
+                    RowTabOpts(title: "Sévère (stade 3-4)", value: 3)
+                  ],
+                  onValueChanged: (int value) {
+                    setState(() {
+                      _encephalopathie = value;
+                    });
+                  }),
+            ],
+          ))
         ],
       ),
     );
--- lib/international/t.dart
+++ lib/international/t.dart
@@ -6,6 +6,12 @@ class vv {
   vv({required this.id, required this.map});
 }
 
+final ASCITE = vv(id: "ascite", map: {Languages.en: "Ascite", Languages.fr: "Ascite"});
+final BILBURINE = vv(id: "bilburine", map: {Languages.en: "Bilburine", Languages.fr: "Bilburine"});
+final ALBUMINE = vv(id: "albumine", map: {Languages.en: "Albumine", Languages.fr: "Albumine"});
+final INR = vv(id: "inr", map: {Languages.en: "Inr", Languages.fr: "Inr"});
+final ENCEPHALOPATHIE = vv(id: "encephalopathie", map: {Languages.en: "Encephalopathie", Languages.fr: "Encéphalopathie"});
+
 final urea = vv(id: "urea", map: {Languages.en: "urea", Languages.fr: "urée"});
 final haemoglobin = vv(id: "haemoglobin", map: {Languages.en: "haemoglobin", Languages.fr: "hemoglobine"});
 final systolicBloodPressure = vv(id: "systolicBloodPressure", map: {Languages.en: "systolic blood pressure", Languages.fr: "Tension artérielle systolique"});
